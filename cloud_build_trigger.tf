3e946f1e1970731eba4c91b7c036bcdd5f77707b
Andrew Milam <andrewmichaelmilam@gmail.com>
2020-12-16 23:51:00 +0000

    making tf more generic

--- cloud_build_trigger.tf
+++ cloud_build_trigger.tf
@@ -4,9 +4,10 @@ resource "google_cloudbuild_trigger" "portfolio_website" {
   provider = "google-beta"
   filename = "cloudbuild.yaml"
   name     = "portfolio-website"
+  project  = var.project
   github {
-    owner = "ammilam"
-    name  = "portfolio-website"
+    owner = var.repo_owner
+    name  = var.repo
     push {
       branch = "^main$"
     }
