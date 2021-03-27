77c8612a3672f7f1760b5838a2558063432e987a
andrew milam <andrewmichaelmilam@gmail.com>
2021-03-27 17:09:33 +0000

    fix

--- cloud_build_trigger.tf
+++ cloud_build_trigger.tf
@@ -1,30 +1,18 @@
 provider "google" {}
 
-data "template_file" "init" {
-  template = "${file("${path.module}/cloudbuild.yaml.tpl")}"
-  vars = {
-    NAME = "${var.name}"
-  }
-}
-
-resource "local_file" "yaml" {
-  content  = data.template_file.init.rendered
-  filename = "cloudbuild.yaml"
-}
 resource "google_cloudbuild_trigger" "portfolio_website" {
-  provider = "google-beta"
-  filename = data.template_file.init.rendered
-  name     = var.name
+  provider = google-beta
+  filename = "cloudbuild.yaml"
+  name     = "portfolio-website"
   project  = var.project
   github {
     owner = var.repo_owner
     name  = var.repo
     push {
-      branch = "feat/cian-website"
+      branch = "main"
     }
 
   }
-  depends_on = [local_file.yaml]
 }
 
 resource "google_project_service" "project" {
