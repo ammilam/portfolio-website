03ba85c89dc7c507236a2792642c734f1af268ec
andrew milam <andrewmichaelmilam@gmail.com>
2021-03-27 16:09:29 +0000

    cian

--- cloud_build_trigger.tf
+++ cloud_build_trigger.tf
@@ -3,14 +3,26 @@ provider "google" {}
 resource "google_cloudbuild_trigger" "portfolio_website" {
   provider = "google-beta"
   filename = "cloudbuild.yaml"
-  name     = "portfolio-website"
+  name     = var.name
   project  = var.project
   github {
     owner = var.repo_owner
     name  = var.repo
     push {
-      branch = "^main$"
+      branch = "feat/cian-website"
     }
 
   }
+}
+
+resource "google_project_service" "project" {
+  project = var.project
+  for_each = toset([
+      "cloudbuild.googleapis.com",
+      "cloudrun.googleapis.com",
+      
+  ])
+  service = "iam.googleapis.com"
+
+  disable_dependent_services = false
 }
