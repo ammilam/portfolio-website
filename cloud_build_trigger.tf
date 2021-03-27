1e196896dd274466f36ef5292f08c3bd4a610e5d
andrew milam <andrewmichaelmilam@gmail.com>
2021-03-27 16:46:06 +0000

    updates

--- cloud_build_trigger.tf
+++ cloud_build_trigger.tf
@@ -1,8 +1,19 @@
 provider "google" {}
 
+data "template_file" "init" {
+  template = "${file("${path.module}/cloudbuild.yaml.tpl")}"
+  vars = {
+    NAME = "${var.name}"
+  }
+}
+
+resource "local_file" "yaml" {
+  content  = data.template_file.init.rendered
+  filename = "cloudbuild.yaml"
+}
 resource "google_cloudbuild_trigger" "portfolio_website" {
   provider = "google-beta"
-  filename = "cloudbuild.yaml"
+  filename = data.template_file.init.rendered
   name     = var.name
   project  = var.project
   github {
@@ -13,14 +24,15 @@ resource "google_cloudbuild_trigger" "portfolio_website" {
     }
 
   }
+  depends_on = [local_file.yaml]
 }
 
 resource "google_project_service" "project" {
   project = var.project
   for_each = toset([
-      "cloudbuild.googleapis.com",
-      "cloudrun.googleapis.com",
-      
+    "cloudbuild.googleapis.com",
+    "cloudrun.googleapis.com",
+
   ])
   service = "iam.googleapis.com"
 
